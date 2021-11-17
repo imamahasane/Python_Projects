c93827c48261d9bc9894d37153ef8227f90a0eda
Md Imam Ahasan <emamahasane@gmail.com>
2021-11-18 00:10:02 +0600

    Add files via upload

---
+++ linkdink.py
@@ -0,0 +1,8 @@
+name = "Md Imam Ahasan"
+intarested = ['Data Science', 
+              'Machine Learning', 
+              'Artificial intelligence', 
+              'Computer vision']
+print(f'Name: {name}\n\tTnterested In:')
+for i, valu in enumerate(intarested):
+    print(f'\t{i + 1}. {valu}')
