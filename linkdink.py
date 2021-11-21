3b27b11ad7f9587106e46d1bf4619be32ce1739c
Md Imam Ahasan <emamahasane@gmail.com>
2021-11-22 01:41:34 +0600

    Delete linkdink.py

--- linkdink.py
+++
@@ -1,8 +0,0 @@
-name = "Md Imam Ahasan"
-intarested = ['Data Science', 
-              'Machine Learning', 
-              'Artificial intelligence', 
-              'Computer vision']
-print(f'Name: {name}\n\tTnterested In:')
-for i, valu in enumerate(intarested):
-    print(f'\t{i + 1}. {valu}')
